8aa291b84f9ee46207e824b150384ff293f2be75
givxq <coooeeeeep@gmail.com>
2023-09-04 12:55:54 +0900

    add SimpleJdbcInsert

--- src/main/kotlin/com/example/itemservice/ItemServiceApplication.kt
+++ src/main/kotlin/com/example/itemservice/ItemServiceApplication.kt
@@ -1,6 +1,6 @@
 package com.example.itemservice
 
-import com.example.itemservice.config.JdbcTemplateV2Config
+import com.example.itemservice.config.JdbcTemplateV3Config
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Import
 val log = KotlinLogging.logger {}
 
 @SpringBootApplication(scanBasePackages = ["com.example.itemservice.web"])
-@Import(JdbcTemplateV2Config::class)
+@Import(JdbcTemplateV3Config::class)
 class ItemServiceApplication
 
 fun main(args: Array<String>) {
